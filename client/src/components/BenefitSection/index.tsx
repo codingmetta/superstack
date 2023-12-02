796de3faea428b2e215397551760e784154b4c62
codingmetta <63598116+codingmetta@users.noreply.github.com>
2023-12-03 00:44:11 +0100

    New Component: BenefitSection finished

--- client/src/components/BenefitSection/index.tsx
+++ client/src/components/BenefitSection/index.tsx
@@ -2,9 +2,9 @@
 
 function BenefitCard({ imgURL, imgAlt, children }) {
     return (
-        <article className='flex flex-col items-center w-40 gap-8 '>
-            <img className="object-contain h-32 " src={imgURL} alt={imgAlt} />
-            <p className="w-32 text-sm font-bold text-center ">{children}</p>
+        <article className='flex flex-col items-center w-40 pb-4 gap-7 '>
+            <img className="object-contain h-36 " src={imgURL} alt={imgAlt} />
+            <p className="w-32 text-sm font-semibold tracking-wide text-center ">{children}</p>
         </article>
 
     )
@@ -12,8 +12,10 @@ function BenefitCard({ imgURL, imgAlt, children }) {
 
 function BenefitSection() {
     return (
-        <section className='flex flex-row flex-wrap justify-around w-full h-full gap-8 bg-white '>
-            <p className='text-xl leading-[1.5rem] text-center tracking-widest uppercase font-climate '>We understood the assigment</p>
+        <section className='flex flex-row flex-wrap justify-around w-full h-full gap-10 px-2.5 py-10 bg-white '>
+            <p className='p-2 text-lg tracking-widest text-center uppercase w-70 font-climate '>
+                We understood the assignment
+                </p>
             <div className='flex flex-row flex-wrap justify-between w-full h-full gap-8'>
                 <BenefitCard imgURL="/images/benefit_1.webp" imgAlt="Metallic Drop" >
                     Hypoallergene Materialien wie Titan, perfekt für sensible Ohren.
@@ -28,7 +30,7 @@ function BenefitSection() {
                     Vertrauenswürdige Partner, die auf faire Bedingungen achten.
                 </BenefitCard>
             </div>
-            <button className='font-bold uppercase'>View moren</button>
+            <button className='w-40 h-10 text-sm font-bold tracking-wider uppercase border border-black rounded-3xl bg-mauve'>View more</button>
         </section>
     )
 }
